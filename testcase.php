946a53e8670e3b97c624453adf249e8ad76ad6f5
qpwoeiru96 <qpwoeiru96@163.com>
2013-04-01 19:41:18 +0800

    fastdfs client test case file

---
+++ testcase.php
@@ -0,0 +1,35 @@
+<?php
+include('FastDFS.php');
+
+$time_start = microtime(TRUE);
+
+$tracker_addr = '10.0.0.5';
+$tracker_port = 22122;
+
+$tracker      = new FastDFS_Taracker($tracker_addr, $tracker_port);
+$storage_info = $tracker->applyStorage('group1');
+
+
+var_dump($storage_info);
+
+$group_name = 'group1';
+$file_path = 'M00/00/00/wKiAgFFY7siAHXjoAK65sGbeVwU223.exe';
+
+$storage = new FastDFS_Storage($storage_info['storage_addr'], $storage_info['storage_port']);
+
+var_dump(
+  //$storage->uploadFile($storage_info['storage_index'], 'F:\\Software\\kugou7403.exe'),
+	//$storage->getFileInfo($group_name, $file_path),
+	//$storage->deleteFile($group_name, $file_path),
+	$storage->setFileMetaData($group_name, $file_path, array(
+	    'time' => time()
+	), 2)
+	//$storage->getFileMetaData($group_name, $file_path)
+	//$storage->downloadFile($group_name, $file_path)
+);
+
+$time_end = microtime(TRUE);
+
+printf("[内存最终使用: %.2fMB]\r\n", memory_get_usage() /1024 /1024 ); 
+printf("[内存最高使用: %.2fMB]\r\n", memory_get_peak_usage()  /1024 /1024) ; 
+printf("[页面执行时间: %.2f毫秒]\r\n", ($time_end - $time_start) * 1000 );
