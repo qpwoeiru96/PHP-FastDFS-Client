30b6df29750dc757240d83a6f8384310b895cb76
qpwoeiru96 <qpwoeiru96@163.com>
2013-04-04 01:29:19 +0800

    Update testcase.php

--- testcase.php
+++ testcase.php
@@ -1,4 +1,6 @@
 <?php
+set_time_limit(10);
+
 include('FastDFS.php');
 
 $time_start = microtime(TRUE);
@@ -9,23 +11,22 @@ $tracker_port = 22122;
 $tracker      = new FastDFS_Taracker($tracker_addr, $tracker_port);
 $storage_info = $tracker->applyStorage('group1');
 
-
-var_dump($storage_info);
-
 $group_name = 'group1';
-$file_path = 'M00/00/00/wKiAgFFY7siAHXjoAK65sGbeVwU223.exe';
+$file_path = 'M00/00/00/CgAABVFYZgmAQ_9nAKnrXobBHdI433.rar';
 
 $storage = new FastDFS_Storage($storage_info['storage_addr'], $storage_info['storage_port']);
 
 var_dump(
-  //$storage->uploadFile($storage_info['storage_index'], 'F:\\Software\\kugou7403.exe'),
-	//$storage->getFileInfo($group_name, $file_path),
-	//$storage->deleteFile($group_name, $file_path),
-	$storage->setFileMetaData($group_name, $file_path, array(
-	    'time' => time()
-	), 2)
-	//$storage->getFileMetaData($group_name, $file_path)
-	//$storage->downloadFile($group_name, $file_path)
+    //$storage->uploadFile($storage_info['storage_index'], 'F:\\Software\\kugou7403.exe'),
+    //$storage->getFileInfo($group_name, $file_path),
+    //$storage->deleteFile($group_name, $file_path),
+    //$storage->setFileMetaData($group_name, $file_path, array(
+    //    'time' => time()
+    //), 2),
+    $storage->uploadSlaveFile('I:\\FastDFS_v4.06\\FastDFS\\HISTORY', $file_path, 'randdom', 'txt'),
+    $storage->getFileInfo($group_name, $file_path)
+    //$storage->getFileMetaData($group_name, $file_path)
+    //$storage->downloadFile($group_name, $file_path)
 );
 
 $time_end = microtime(TRUE);
