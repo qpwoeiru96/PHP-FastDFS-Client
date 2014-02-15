fcf9a41ff555282bf9f8cd8546422a9a9858364c
qpwoeiru96 <qpwoeiru96@gmail.com>
2014-02-15 11:07:44 +0800

    小版本更新一下
    项目中也用到了 如果有Bug会及时更新 暂停维护 够用就行

--- testcase.php
+++ testcase.php
@@ -1,32 +1,43 @@
 <?php
 set_time_limit(10);
 
-include('FastDFS.php');
+include(__DIR__ . '/Exception.php');
+include(__DIR__ . '/Base.php');
+include(__DIR__ . '/Tracker.php');
+include(__DIR__ . '/Storage.php');
 
 $time_start = microtime(TRUE);
 
-$tracker_addr = '10.0.0.5';
+$tracker_addr = '168.192.122.29';
 $tracker_port = 22122;
 
-$tracker      = new FastDFS_Tracker($tracker_addr, $tracker_port);
-$storage_info = $tracker->applyStorage('group1');
+$tracker      = new FastDFS\Tracker($tracker_addr, $tracker_port);
+$storage_info = $tracker->applyStorage('lms');
 
-$group_name = 'group1';
-$file_path = 'M00/00/00/CgAABVFYZgmAQ_9nAKnrXobBHdI433.rar';
+var_dump($storage_info);
 
-$storage = new FastDFS_Storage($storage_info['storage_addr'], $storage_info['storage_port']);
+
+$group_name = 'lms';
+//$file_path = 'M00/00/00/CgAABVFYZgmAQ_9nAKnrXobBHdI433.rar';
+//$appender_file_path = 'M00/00/00/CgAABVFc8duEOo6HAAAAAD1cKVQ817.txt';
+
+$storage = new FastDFS\Storage($storage_info['storage_addr'], $storage_info['storage_port']);
 
 var_dump(
-    //$storage->uploadFile($storage_info['storage_index'], 'F:\\Software\\kugou7403.exe'),
+    $storage->downloadToFile('lms', 'M00/00/00/qMB6HVLYiECAJ2KTACvD-UdfQeU480.pdf', __DIR__ . '/test.pdf')
+    //$storage->uploadFile($storage_info['storage_index'], 'F:\\Downloads\\desktop.ini')
     //$storage->getFileInfo($group_name, $file_path),
     //$storage->deleteFile($group_name, $file_path),
     //$storage->setFileMetaData($group_name, $file_path, array(
     //    'time' => time()
     //), 2),
-    $storage->uploadSlaveFile('I:\\FastDFS_v4.06\\FastDFS\\HISTORY', $file_path, 'randdom', 'txt'),
-    $storage->getFileInfo($group_name, $file_path)
+    //$storage->uploadSlaveFile('I:\\FastDFS_v4.06\\FastDFS\\HISTORY', $file_path, 'randdom', 'txt'),
+    //$storage->getFileInfo($group_name, $file_path)
     //$storage->getFileMetaData($group_name, $file_path)
     //$storage->downloadFile($group_name, $file_path)
+    //$storage->uploadAppenderFile($storage_info['storage_index'], 'I:\\FastDFS_v4.06\\FastDFS\\HISTORY', 'txt')
+    //$storage->appendFile('TEST' . time() . PHP_EOL, $appender_file_path)
+    //$storage->modifyFile('I:\\FastDFS_v4.06\\FastDFS\\INSTALL', $appender_file_path, 0)
 );
 
 $time_end = microtime(TRUE);
